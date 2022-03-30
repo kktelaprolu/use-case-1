b81a0b3849570465815fe3fd132767ace896cb40
kktelaprolu <kktelaprolu@gmail.com>
2022-03-30 06:25:00 +0000

    all spanner instances deleted

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,3 +20,14 @@ module spanner {
   force_destroy			= var.force_destroy
   databases			= var.databases  
 }
+
+module spanner {
+  source = "../"
+  name				= var.instance_name
+  config			= var.config
+  display_name		= var.display_name
+  num_nodes			= var.num_nodes
+  labels			= var.labels
+  project			= var.project
+  force_destroy		= var.force_destroy
+}
