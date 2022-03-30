1b9a0f3433e3995f90efd2ca95065b0725a87277
kktelaprolu <kktelaprolu@gmail.com>
2022-03-30 07:10:51 +0000

    name modified for spanner2

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,7 +38,7 @@ module spanner {
 
 module spanner2 {
   source = "./regional_deployment/spanner"
-  name				= "spanner-test-instance-2"
+  instance_name				= "spanner-test-instance-2"
   config			= "regional-europe-west1"
   display_name			= "tf-spanner-test-instance-2"
   num_nodes			= 1
